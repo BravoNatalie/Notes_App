47aad3c4c0186894a7b6f3fd1f77b92bf92d9bf6
bravonatalie <natalie.bravo@outlook.com>
2021-03-25 20:09:50 -0300

    Adding our Serverless API

---
+++ notes-api/libs/dynamodb-lib.js
@@ -0,0 +1,12 @@
+import AWS from "aws-sdk";
+
+//AWS.config.update({ region: "us-east-2" });
+const client = new AWS.DynamoDB.DocumentClient();
+
+export default {
+  get: (params) => client.get(params).promise(),
+  put: (params) => client.put(params).promise(),
+  query: (params) => client.query(params).promise(),
+  update: (params) => client.update(params).promise(),
+  delete: (params) => client.delete(params).promise(),
+};
