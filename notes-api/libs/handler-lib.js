e298617255948ef8c7887c273448dbf332d5aa3c
bravonatalie <natalie.bravo@outlook.com>
2021-03-30 17:14:47 -0300

    Adding CORS to the Serverless API

--- notes-api/libs/handler-lib.js
+++ notes-api/libs/handler-lib.js
@@ -15,6 +15,10 @@ export default function handler(lambda) {
     return {
       statusCode,
       body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     };
   };
 }
